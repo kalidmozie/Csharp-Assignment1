07a8b105c749601e29b06fd05ad39bed42d95bff
Biniyam <65971570+kalidmozie@users.noreply.github.com>
2022-07-05 09:54:33 +0300

    EX- 2
    Exercise 2

---
+++ statement/statement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace statement 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index < 100) 
+
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
